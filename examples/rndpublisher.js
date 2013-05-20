59e3d9987cacb6f3f076fc87c83e4411e24196e7
dashersw <armagan@amcalar.com>
2013-05-20 13:05:39 +0300

    Adds "random" namespaced publisher / responder examples.

---
+++ examples/rndpublisher.js
@@ -0,0 +1,22 @@
+var Publisher = require('../').Publisher;
+
+// Instantiate a new Publisher component.
+var randomPublisher = new Publisher({
+    name: 'randomPub',
+    namespace: 'rnd',
+    broadcasts: ['randomUpdate']
+});
+
+// Wait for the publisher to find an open port and listen on it.
+randomPublisher.on('ready', function() {
+    setInterval(function() {
+        var val = {
+            val: ~~(Math.random() * 1000)
+        };
+
+        console.log('emitting', val);
+
+        // publish an event with arbitrary data at any time
+        randomPublisher.publish('randomUpdate', val);
+    }, 3000);
+});
---
+++ examples/rndresponder.js
@@ -0,0 +1,16 @@
+var Responder = require('../').Responder;
+
+// Instantiate a new Responder component.
+var randomResponder = new Responder({
+    name: 'randomRep',
+    namespace: 'rnd',
+    respondsTo: ['randomRequest'] // types of requests this responder
+    							  // can respond to.
+});
+
+// request handlers are like any event handler.
+randomResponder.on('randomRequest', function(req, cb) {
+    var answer = ~~(Math.random() * 10);
+    console.log('request', req.data, 'answering with', answer);
+    cb(answer);
+});
