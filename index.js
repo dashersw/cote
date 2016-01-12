ba9d08a615730a53480dd026865f84d69d504797
dashersw <armagan@amcalar.com>
2016-01-12 16:20:22 +0100

    Let cote handle default discovery options (fixes #14)
    var cote = require('cote'); works as normally. To provide default
    discovery options, one can use, var cote = require('cote')(options)
    where options is a JavaScript object.

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-module.exports = {
-    Requester: require('./lib/Requester.js'),
-    Responder: require('./lib/Responder.js'),
-    Publisher: require('./lib/Publisher.js'),
-    Subscriber: require('./lib/Subscriber.js'),
-    Sockend: require('./lib/Sockend.js'),
-    Monitor: require('./lib/Monitor.js'),
-    TimeBalancedRequester: require('./lib/TimeBalancedRequester.js'),
-    PendingBalancedRequester: require('./lib/PendingBalancedRequester.js')
-};
+var Discovery = require('./lib/Discovery'),
+    Requester = require('./lib/Requester.js'),
+    Responder = require('./lib/Responder.js'),
+    Publisher = require('./lib/Publisher.js'),
+    Subscriber = require('./lib/Subscriber.js'),
+    Sockend = require('./lib/Sockend.js'),
+    Monitor = require('./lib/Monitor.js'),
+    TimeBalancedRequester = require('./lib/TimeBalancedRequester.js'),
+    PendingBalancedRequester = require('./lib/PendingBalancedRequester.js');
+
+function cote(discoveryOptions) {
+    Discovery.setDefaults(discoveryOptions);
+
+    return cote;
+}
+
+cote.Requester = Requester;
+cote.Responder = Responder;
+cote.Publisher = Publisher;
+cote.Subscriber = Subscriber;
+cote.Sockend = Sockend;
+cote.Monitor = Monitor;
+cote.TimeBalancedRequester = TimeBalancedRequester;
+cote.PendingBalancedRequester = PendingBalancedRequester;
+
+module.exports = cote;
