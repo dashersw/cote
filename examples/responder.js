2a7ceeb5a176858e1dba5918ae6fc83a36251b7a
Armagan Amcalar <armagan@amcalar.com>
2017-05-27 21:49:56 +0200

    Lint fixes

--- examples/responder.js
+++ examples/responder.js
@@ -1,23 +1,23 @@
-var Responder = require('../').Responder;
+let Responder = require('../src').Responder;
 
 // Instantiate a new Responder component.
-var randomResponder = new Responder({
+let randomResponder = new Responder({
     name: 'randomRep',
     // namespace: 'rnd',
-    respondsTo: ['randomRequest', 'promised request'] // types of requests this responder
+    respondsTo: ['randomRequest', 'promised request'], // types of requests this responder
     							  // can respond to.
 });
 
 // request handlers are like any event handler.
 randomResponder.on('randomRequest', function(req, cb) {
-    var answer = ~~(Math.random() * 10);
+    let answer = ~~(Math.random() * 10);
     console.log('request', req.val, 'answering with', answer);
     cb(answer);
 });
 
 // request handlers are like any event handler.
 randomResponder.on('promised request', function(req) {
-    var answer = ~~(Math.random() * 10);
+    let answer = ~~(Math.random() * 10);
 
     return new Promise((resolve, reject) => {
         console.log('promise request', req.val, 'answering with', answer);
--- src/monitoring-tool/index.js
+++ src/monitoring-tool/index.js
@@ -1,64 +1,64 @@
+'use strict';
+
 module.exports = function(port) {
-    var fs = require('fs'),
+    let fs = require('fs'),
         _ = require('lodash'),
         cote = require('../');
 
-    var app = require('http').createServer(handler),
+    let app = require('http').createServer(handler),
         io = require('socket.io').listen(app);
 
     // Instantiate a monitor, sockend and publisher components
-    var monitor = new cote.Monitor({
-        name: 'monitor'
-    }, {disableScreen: true});
+    let monitor = new cote.Monitor({
+        name: 'monitor',
+    }, { disableScreen: true });
 
-    var sockend = new cote.Sockend(io, {
+    let sockend = new cote.Sockend(io, {
         name: 'sockend',
         namespace: 'monitoring',
-        key: 'monitoring'
+        key: 'monitoring',
     });
 
-    var publisher = new cote.Publisher({
+    let publisher = new cote.Publisher({
         name: 'status publisher',
         broadcasts: ['statusUpdate'],
         namespace: 'monitoring',
-        key: 'monitoring'
+        key: 'monitoring',
     });
 
-// Graph related variables
-    var graph = {
+    // Graph related variables
+    let graph = {
         nodes: [],
-        links: []
+        links: [],
     };
-    var rawLinks = {};
+    let rawLinks = {};
 
-// Sockend
+    // Sockend
     app.listen(port || 5555);
 
-
     monitor.on('status', function(status) {
-        var node = monitor.discovery.nodes[status.id];
+        let node = monitor.discovery.nodes[status.id];
         if (!node) return;
 
         if (node.processId == monitor.discovery.me.processId) return;
 
         rawLinks[status.id] = {
             source: status.id,
-            target: status.nodes
+            target: status.nodes,
         };
     });
 
     monitor.discovery.on('removed', function(node) {
         delete rawLinks[node.id];
-        var removedNode = node.id;
+        let removedNode = node.id;
 
-        for (nodeId in rawLinks) {
-            var rawLink = rawLinks[nodeId];
+        for (let nodeId in rawLinks) {
+            let rawLink = rawLinks[nodeId];
 
-            var removedNodeIndex = rawLink.target.indexOf(removedNode);
+            let removedNodeIndex = rawLink.target.indexOf(removedNode);
             if (removedNodeIndex > -1) {
                 rawLink.target.splice(removedNodeIndex, 1);
-                if (!rawLink.target.length)
-                    delete rawLinks[nodeId];
+                if (!rawLink.target.length) delete rawLinks[nodeId];
             }
         }
     });
@@ -66,17 +66,17 @@ module.exports = function(port) {
     setInterval(function() {
         graph.nodes = [];
 
-        var hosts = getHosts(monitor.discovery.nodes);
+        let hosts = getHosts(monitor.discovery.nodes);
         graph.nodes = graph.nodes.concat(hosts);
 
-        var processes = getProcesses(monitor.discovery.nodes);
+        let processes = getProcesses(monitor.discovery.nodes);
         graph.nodes = graph.nodes.concat(processes);
 
-        var nodes = getNodes(monitor.discovery.nodes);
+        let nodes = getNodes(monitor.discovery.nodes);
         graph.nodes = graph.nodes.concat(nodes);
 
         // Update links
-        var indexMap = {};
+        let indexMap = {};
         graph.nodes.forEach(function(node, index) {
             indexMap[node.id] = index;
         });
@@ -86,38 +86,36 @@ module.exports = function(port) {
         publisher.publish('statusUpdate', graph);
     }, 5000);
 
-
     function handler(req, res) {
-        fs.readFile(__dirname + '/frontend/index.html',
-            function(err, data) {
-                if (err) {
-                    res.writeHead(500);
-                    return res.end('Error loading index.html');
-                }
-                res.writeHead(200);
-                res.end(data);
-            });
+        fs.readFile(__dirname + '/frontend/index.html', function(err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            res.writeHead(200);
+            res.end(data);
+        });
     }
 
     function getProcesses(nodes) {
-        var processes = _.groupBy(nodes, 'processId');
+        let processes = _.groupBy(nodes, 'processId');
 
         return _.map(processes, function(process, processId) {
             return {
                 id: processId,
                 type: 'process',
-                name: process[0].processCommand
-            }
+                name: process[0].processCommand,
+            };
         }).filter(function(process) {
             return process.id != monitor.discovery.me.processId;
         });
     }
 
     function getHosts(nodes) {
-        var nodesByHosts = _.groupBy(nodes, 'hostName');
+        let nodesByHosts = _.groupBy(nodes, 'hostName');
 
         _.forEach(nodesByHosts, function(nodesByHost, hostId) {
-            var nodesByProcess = _.groupBy(nodesByHost, 'processId');
+            let nodesByProcess = _.groupBy(nodesByHost, 'processId');
 
             _.forEach(nodesByProcess, function(processNodes, processId) {
                 if (processId == monitor.discovery.me.processId) return;
@@ -126,22 +124,22 @@ module.exports = function(port) {
                     source: processId,
                     target: processNodes.map(function(node) {
                         return node.id;
-                    })
-                }
+                    }),
+                };
             });
 
             rawLinks[hostId] = {
                 source: hostId,
-                target: Object.keys(nodesByProcess)
+                target: Object.keys(nodesByProcess),
             };
         });
 
-        var hosts = Object.keys(nodesByHosts);
+        let hosts = Object.keys(nodesByHosts);
         hosts = _.map(hosts, function(host) {
             return {
                 id: host,
                 type: 'host',
-                name: host
+                name: host,
             };
         });
         return hosts;
@@ -150,14 +148,15 @@ module.exports = function(port) {
     function getNodes(nodes) {
         nodes = _.filter(nodes, function(node) {
             return node.processId != monitor.discovery.me.processId &&
-                node.advertisement.name != 'sockendSub' && node.advertisement.name != 'sockendReq';
+                node.advertisement.name != 'sockendSub' &&
+                node.advertisement.name != 'sockendReq';
         });
 
-        simplifiedNodes = _.map(nodes, function(node) {
+        const simplifiedNodes = _.map(nodes, function(node) {
             return {
                 id: node.id,
                 type: 'node',
-                name: node.advertisement.name
+                name: node.advertisement.name,
             };
         });
 
@@ -165,11 +164,11 @@ module.exports = function(port) {
     }
 
     function getLinks(rawLinks, indexMap) {
-        var links = _.map(rawLinks, function(rawLink) {
+        let links = _.map(rawLinks, function(rawLink) {
             return rawLink.target.map(function(target) {
                 return { // flip source & target for semantics :)
-                    source: indexMap[target],//monitor.discovery.nodes[target].advertisement.name + '#' + target,
-                    target: indexMap[rawLink.source]//monitor.discovery.nodes[rawLink.source].advertisement.name + '#' + rawLink.source
+                    source: indexMap[target], // monitor.discovery.nodes[target].advertisement.name + '#' + target,
+                    target: indexMap[rawLink.source], // monitor.discovery.nodes[rawLink.source].advertisement.name + '#' + rawLink.source
                 };
             });
         });
