22d7f6b479cb62a8d42f593bca6fda9fe0adc78b
Armagan Amcalar <armagan@amcalar.com>
2018-12-24 04:45:52 +0100

    Refactor discovery options configuration and fix undefined overrides

--- src/index.js
+++ src/index.js
@@ -8,51 +8,10 @@ const Monitor = require('./components/monitor');
 const MonitoringTool = require('./monitoring-tool');
 const TimeBalancedRequester = require('./components/time-balanced-requester');
 const PendingBalancedRequester = require('./components/pending-balanced-requester');
-
-const parser = require('../lib/env-var-parser');
-
-const defaultOptions = {
-    environment: '',
-    useHostNames: false,
-    broadcast: null,
-    multicast: null,
-};
+const optionsBuilder = require('./options-builder');
 
 const cote = (options = {}) => {
-    const environmentSettings = {
-        environment: process.env.COTE_ENV,
-        useHostNames: !!process.env.COTE_USE_HOST_NAMES,
-        broadcast: process.env.COTE_BROADCAST_ADDRESS ||
-        (process.env.DOCKERCLOUD_IP_ADDRESS ? '10.7.255.255' : undefined),
-        multicast: process.env.COTE_MULTICAST_ADDRESS,
-        checkInterval: parser.int(process.env.COTE_CHECK_INTERVAL),
-        helloInterval: parser.int(process.env.COTE_HELLO_INTERVAL),
-        helloLogsEnabled: parser.bool(process.env.COTE_HELLO_LOGS_ENABLED),
-        statusLogsEnabled: parser.bool(process.env.COTE_STATUS_LOGS_ENABLED),
-        log: parser.bool(process.env.COTE_LOG),
-        nodeTimeout: parser.int(process.env.COTE_NODE_TIMEOUT),
-    };
-
-    const keys = Object.keys(process.env).filter((k) => k.slice(0, 15) == 'COTE_DISCOVERY_');
-
-    keys.forEach((k) => {
-        const keyName = k.slice(15);
-        const keyArray = keyName.split('_').map((k) => k.toLowerCase());
-        const pluginName = keyArray.shift();
-
-        const pluginObj = environmentSettings[pluginName] = environmentSettings[pluginName] || {};
-
-        keyArray.forEach((k) => {
-            pluginObj[k] = process.env[`COTE_DISCOVERY_${pluginName.toUpperCase()}_${k.toUpperCase()}`];
-        });
-
-        // Discovery plugins (such as redis) may not have access to real IP addresses.
-        // Therefore we automatically default to `true` for `COTE_USE_HOST_NAMES`,
-        // since host names are accurate.
-        environmentSettings.useHostNames = true;
-    });
-
-    options = { ...defaultOptions, ...environmentSettings, ...options };
+    options = optionsBuilder(options);
 
     Discovery.setDefaults(options);
 
---
+++ src/options-builder.js
@@ -0,0 +1,62 @@
+const parser = {
+  bool: (v) => v != undefined ? v.toLowerCase() == 'true' : undefined,
+  int: (v) => v != undefined ? parseInt(v, 10) : undefined,
+  str: (v) => v,
+  exists: (v) => !!v,
+};
+
+const defaultOptions = {
+  environment: '',
+  useHostNames: false,
+  broadcast: null,
+  multicast: null,
+};
+
+const envVarOptionsMap = {
+  COTE_ENV: ['environment', parser.str],
+  COTE_USE_HOST_NAMES: ['useHostNames', parser.exists],
+  COTE_MULTICAST_ADDRESS: ['multicast', parser.str],
+  COTE_CHECK_INTERVAL: ['checkInterval', parser.int],
+  COTE_HELLO_INTERVAL: ['helloInterval', parser.int],
+  COTE_HELLO_LOGS_ENABLED: ['helloLogsEnabled', parser.bool],
+  COTE_STATUS_LOGS_ENABLED: ['statusLogsEnabled', parser.bool],
+  COTE_LOG: ['log', parser.bool],
+  COTE_NODE_TIMEOUT: ['nodeTimeout', parser.int],
+};
+
+module.exports = (options = {}) => {
+  const environmentSettings = {};
+
+  Object.entries(envVarOptionsMap).forEach(([envVar, [setting, parser]]) => {
+    if (!(envVar in process.env)) return;
+
+    const value = process.env[envVar];
+
+    environmentSettings[setting] = parser(value);
+  });
+
+  if (!process.env.COTE_BROADCAST_ADDRESS && process.env.DOCKERCLOUD_IP_ADDRESS) {
+    environmentSettings.broadcast = '10.7.255.255';
+  }
+
+  const keys = Object.keys(process.env).filter((k) => k.slice(0, 15) == 'COTE_DISCOVERY_');
+
+  keys.forEach((k) => {
+    const keyName = k.slice(15);
+    const keyArray = keyName.split('_').map((k) => k.toLowerCase());
+    const pluginName = keyArray.shift();
+
+    const pluginObj = environmentSettings[pluginName] = environmentSettings[pluginName] || {};
+
+    keyArray.forEach((k) => {
+      pluginObj[k] = process.env[`COTE_DISCOVERY_${pluginName.toUpperCase()}_${k.toUpperCase()}`];
+    });
+
+    // Discovery plugins (such as redis) may not have access to real IP addresses.
+    // Therefore we automatically default to `true` for `COTE_USE_HOST_NAMES`,
+    // since host names are accurate.
+    environmentSettings.useHostNames = true;
+  });
+
+  return { ...defaultOptions, ...environmentSettings, ...options };
+};
