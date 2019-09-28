179d1e918cc83f4ba5470db333fd1c97b71867bd
Armagan Amcalar <armagan@amcalar.com>
2019-09-28 17:26:06 +0200

    Build

--- dist/components/discovery.js
+++ dist/components/discovery.js
@@ -1,10 +1,14 @@
 "use strict";
 
+require("core-js/modules/es7.object.get-own-property-descriptors");
+
 require("core-js/modules/es6.symbol");
 
 require("core-js/modules/es6.object.to-string");
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -27,7 +31,7 @@ const defaultOptions = {
 
 class Discovery extends Discover {
   constructor(advertisement, options) {
-    options = _objectSpread({}, defaultOptions, Discovery.defaults, options);
+    options = _objectSpread({}, defaultOptions, {}, Discovery.defaults, {}, options);
     super(options);
     this.advertisement = _objectSpread({
       type: 'service'
--- dist/options-builder.js
+++ dist/options-builder.js
@@ -1,12 +1,16 @@
 "use strict";
 
+require("core-js/modules/es7.object.get-own-property-descriptors");
+
 require("core-js/modules/es6.symbol");
 
 require("core-js/modules/es6.object.to-string");
 
 require("core-js/modules/es7.object.entries");
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -65,6 +69,6 @@ module.exports = options => {
 
     environmentSettings.useHostNames = true;
   });
-  return _objectSpread({}, defaultOptions, environmentSettings, options);
+  return _objectSpread({}, defaultOptions, {}, environmentSettings, {}, options);
 };
 //# sourceMappingURL=options-builder.js.map
